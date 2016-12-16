785dba61c4930fd3002d75d7295d95737a85105a
stephan <stephan@mbp-di-stephan.homenet.telecomitalia.it>
2016-12-16 13:42:10 +0100

    Implement Class Showcase

--- classes/Showcase.php
+++ classes/Showcase.php
@@ -1,8 +1,41 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Class of Showcase. The goal is to show the connection between Showcase that
+ * contain Advertise published and unpublished
+ * 
  */
+class Showcase {
 
+  protected $advertising;
+  protected $isPublished;
+
+  
+  // Showcase to be alive needs an advertising
+  public function __construct(Advertising $advertising) {
+    $this->advertising = $advertising;
+    echo "constructor advertising ok<br>";
+  }
+
+  public function unPublishAdvertising($advertising = true) {
+    if ($advertising === true) {
+
+      $this->advertising->unPublish();
+    }
+    
+    $this->isPublished = false;
+    
+    echo "advertising unpublished<br>";
+  }
+  
+  public function publishAdvertising() {
+    if ($this->advertising->isPublished() == false) {
+
+      $this->advertising->publish();
+    }
+    
+    $this->isPublished = true;
+    echo "advertising published<br>";
+  }
+
+}
